b2b1c4b5cf09ce86932871940a5381f2e5fe7a5b
Stephen Prochnow <stephen.prochnow@gmail.com>
2015-06-23 19:45:31 +0200

    Added service to activity and implemented fragment transition

--- app/src/main/java/com/ttsnotification/prochnow/ttsnotifications/MainActivity.java
+++ app/src/main/java/com/ttsnotification/prochnow/ttsnotifications/MainActivity.java
@@ -1,20 +1,43 @@
 package com.ttsnotification.prochnow.ttsnotifications;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
 
-    DrawerLayout drawerLayout;
-    ActionBarDrawerToggle drawerToggle;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
     private Toolbar toolbar;
+
+    @InjectView(R.id.navigation) NavigationView navigation;
+    @InjectView(R.id.contentArea) NestedScrollView contentArea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +45,14 @@ public class MainActivity extends AppCompatActivity {
         //setTheme(R.style.AppThemeLight_WithoutActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent checkTTSIntent = new Intent();
+        checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkTTSIntent, MY_DATA_CHECK_CODE);
+        LocalBroadcastManager.getInstance(this).registerReceiver(onNotice, new IntentFilter("Msg"));
+
+
+        ButterKnife.inject(this);
         initToolbar();
         initInstances();
     }
@@ -57,7 +88,6 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -71,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
         drawerToggle.syncState();
     }
 
+
     private void initInstances() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         drawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.hello_world, R.string.hello_world);
@@ -79,5 +110,102 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        doFragmentTransition(new HomeFragment(), getString(R.string.homeTitle));
+
+
+        navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                drawerLayout.closeDrawers();
+                switch (id) {
+                    case R.id.homeMenu:
+
+                        doFragmentTransition(new HomeFragment(), getString(R.string.homeTitle));
+
+                        break;
+                    case R.id.appMenu:
+                        doFragmentTransition(new AppFragment(), getString(R.string.appTitle));
+                        break;
+                    case R.id.wifiMenu:
+
+                        doFragmentTransition(new WifiFragment(), getString(R.string.wifiTitle));
+                        break;
+                    case R.id.locationMenu:
+                        doFragmentTransition(new LocationFragment(), getString(R.string.locationTitle));
+                        break;
+                }
+                return false;
+            }
+        });
     }
+
+    private void doFragmentTransition(Fragment fragment, String newActionBarTitle) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+
+        getSupportActionBar().setTitle(newActionBarTitle);
+        fragmentTransaction.replace(R.id.contentArea, fragment, null);
+        fragmentTransaction.commit();
+    }
+
+    //TTS object
+    private TextToSpeech myTTS;
+    //status check code
+    private int MY_DATA_CHECK_CODE = 0;
+
+    private BroadcastReceiver onNotice = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String pack = intent.getStringExtra("package");
+            String title = intent.getStringExtra("title");
+            String text = intent.getStringExtra("text");
+
+            Log.d(LOG_TAG, "onReceive ");
+//            TableRow tr = new TableRow(getApplicationContext());
+//            tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+//            TextView textview = new TextView(getApplicationContext());
+//            textview.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1.0f));
+//            textview.setTextSize(20);
+//            textview.setTextColor(Color.parseColor("#0B0719"));
+//            textview.setText(Html.fromHtml(pack + "<br><b>" + title + " : </b>" + text));
+//            tr.addView(textview);
+//            tab.addView(tr);
+
+            myTTS.speak(title, TextToSpeech.QUEUE_FLUSH, null);
+
+
+        }
+    };
+
+
+    //act on result of TTS data check
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == MY_DATA_CHECK_CODE) {
+            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
+                //the user has the necessary data - create the TTS
+                myTTS = new TextToSpeech(this, this);
+            } else {
+                //no data - install it now
+                Intent installTTSIntent = new Intent();
+                installTTSIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+                startActivity(installTTSIntent);
+            }
+        }
+    }
+
+    //setup TTS
+    public void onInit(int initStatus) {
+
+        //check for successful instantiation
+        if (initStatus == TextToSpeech.SUCCESS) {
+            if (myTTS.isLanguageAvailable(Locale.US) == TextToSpeech.LANG_AVAILABLE)
+                myTTS.setLanguage(Locale.US);
+        } else if (initStatus == TextToSpeech.ERROR) {
+            Toast.makeText(this, "Sorry! Text To Speech failed...", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
