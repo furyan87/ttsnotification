4083c72db5774439ed2fc2aa93eb3cd7c6e8f75c
Stephen Prochnow <stephen.prochnow@gmail.com>
2015-06-24 15:04:10 +0200

    Added dedicated service for TTS

--- app/src/main/java/com/ttsnotification/prochnow/ttsnotifications/MainActivity.java
+++ app/src/main/java/com/ttsnotification/prochnow/ttsnotifications/MainActivity.java
@@ -1,43 +1,31 @@
 package com.ttsnotification.prochnow.ttsnotifications;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
-
-import java.util.Locale;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+public class MainActivity extends AppCompatActivity {
 
     private final String LOG_TAG = MainActivity.class.getSimpleName();
 
 
-    private DrawerLayout drawerLayout;
+    @InjectView(R.id.drawerLayout) DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
-    private Toolbar toolbar;
-
+    @InjectView(R.id.toolbar) Toolbar toolbar;
     @InjectView(R.id.navigation) NavigationView navigation;
-    @InjectView(R.id.contentArea) NestedScrollView contentArea;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,20 +34,10 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent checkTTSIntent = new Intent();
-        checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
-        startActivityForResult(checkTTSIntent, MY_DATA_CHECK_CODE);
-        LocalBroadcastManager.getInstance(this).registerReceiver(onNotice, new IntentFilter("Msg"));
-
 
         ButterKnife.inject(this);
-        initToolbar();
-        initInstances();
-    }
-
-    private void initToolbar() {
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        initInstances();
     }
 
     @Override
@@ -103,7 +81,6 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
 
 
     private void initInstances() {
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         drawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.hello_world, R.string.hello_world);
         drawerLayout.setDrawerListener(drawerToggle);
 
@@ -144,68 +121,10 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-
         getSupportActionBar().setTitle(newActionBarTitle);
         fragmentTransaction.replace(R.id.contentArea, fragment, null);
         fragmentTransaction.commit();
     }
 
-    //TTS object
-    private TextToSpeech myTTS;
-    //status check code
-    private int MY_DATA_CHECK_CODE = 0;
-
-    private BroadcastReceiver onNotice = new BroadcastReceiver() {
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String pack = intent.getStringExtra("package");
-            String title = intent.getStringExtra("title");
-            String text = intent.getStringExtra("text");
-
-            Log.d(LOG_TAG, "onReceive ");
-//            TableRow tr = new TableRow(getApplicationContext());
-//            tr.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
-//            TextView textview = new TextView(getApplicationContext());
-//            textview.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1.0f));
-//            textview.setTextSize(20);
-//            textview.setTextColor(Color.parseColor("#0B0719"));
-//            textview.setText(Html.fromHtml(pack + "<br><b>" + title + " : </b>" + text));
-//            tr.addView(textview);
-//            tab.addView(tr);
-
-            myTTS.speak(title, TextToSpeech.QUEUE_FLUSH, null);
-
-
-        }
-    };
-
-
-    //act on result of TTS data check
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == MY_DATA_CHECK_CODE) {
-            if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
-                //the user has the necessary data - create the TTS
-                myTTS = new TextToSpeech(this, this);
-            } else {
-                //no data - install it now
-                Intent installTTSIntent = new Intent();
-                installTTSIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
-                startActivity(installTTSIntent);
-            }
-        }
-    }
-
-    //setup TTS
-    public void onInit(int initStatus) {
-
-        //check for successful instantiation
-        if (initStatus == TextToSpeech.SUCCESS) {
-            if (myTTS.isLanguageAvailable(Locale.US) == TextToSpeech.LANG_AVAILABLE)
-                myTTS.setLanguage(Locale.US);
-        } else if (initStatus == TextToSpeech.ERROR) {
-            Toast.makeText(this, "Sorry! Text To Speech failed...", Toast.LENGTH_LONG).show();
-        }
-    }
 
 }
---
+++ app/src/main/java/com/ttsnotification/prochnow/ttsnotifications/TTSService.java
@@ -0,0 +1,86 @@
+package com.ttsnotification.prochnow.ttsnotifications;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
+
+import java.util.HashMap;
+import java.util.Locale;
+
+/**
+ * Created by prochnow on 24.06.15.
+ */
+public class TTSService extends Service implements TextToSpeech.OnInitListener {
+
+    private static boolean TTSFLAG = false;
+    private final String LOG_TAG = TTSService.class.getSimpleName();
+
+    private TextToSpeech mTts;
+    private String spokenText;
+
+    HashMap<String, String> map = new HashMap<String, String>();
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        spokenText = intent.getStringExtra("msgText");
+
+        if (TTSFLAG) {
+            mTts.speak(spokenText, TextToSpeech.QUEUE_ADD, map);
+        }
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+        mTts = new TextToSpeech(this, this);
+        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "UniqueID");
+        mTts.setOnUtteranceProgressListener(new NotificationUtteranceProgressListener());
+        // This is a good place to set spokenText
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = mTts.setLanguage(Locale.US);
+            if (result != TextToSpeech.LANG_MISSING_DATA && result != TextToSpeech.LANG_NOT_SUPPORTED) {
+                mTts.speak(spokenText, TextToSpeech.QUEUE_FLUSH, map);
+
+                TTSFLAG = true;
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mTts != null) {
+            mTts.stop();
+            mTts.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+
+    private class NotificationUtteranceProgressListener extends UtteranceProgressListener {
+        @Override
+        public void onStart(String s) {
+
+        }
+
+        @Override
+        public void onDone(String s) {
+
+        }
+
+        @Override
+        public void onError(String s) {
+
+        }
+    }
+}
