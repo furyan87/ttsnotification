49cb6f4a867d34d8279cab77d9d463326a61b69a
Stephen Prochnow <stephen.prochnow@gmail.com>
2015-06-25 14:56:07 +0200

    Added listing of installed apps as test

--- app/src/main/java/com/prochnow/ttsnotifications/app/AddAppFragment.java
+++ app/src/main/java/com/prochnow/ttsnotifications/app/AddAppFragment.java
@@ -1,7 +1,12 @@
 package com.prochnow.ttsnotifications.app;
 
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,17 +14,49 @@ import android.view.ViewGroup;
 import com.prochnow.ttsnotifications.MainActivity;
 import com.prochnow.ttsnotifications.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by prochnow on 25.06.15.
  */
 public class AddAppFragment extends Fragment {
 
+    private final String LOG_TAG = AddAppFragment.class.getSimpleName();
+
+    @InjectView(R.id.appListView) RecyclerView appListView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    List<String> strings;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        initInstances();
+    }
+
+    private void initInstances() {
+
+        strings = new ArrayList<>();
+        final Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        final List<ApplicationInfo> pkgAppsList = getActivity().getPackageManager().getInstalledApplications(0);
+        for (ApplicationInfo info : pkgAppsList) {
+            Log.d(LOG_TAG, "ApplicationInfoName: " + getActivity().getPackageManager().getApplicationLabel(info));
+            Log.d(LOG_TAG, "ApplicationIcon: " + getActivity().getPackageManager().getApplicationIcon(info));
+            Log.d(LOG_TAG, "ApplicationPackage: " + info.packageName);
+
+
+            strings.add(getActivity().getPackageManager().getApplicationLabel(info).toString());
+        }
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+
     }
 
     @Override
@@ -27,6 +64,16 @@ public class AddAppFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_addapp, container, false);
         ButterKnife.inject(this, rootView);
+
+        appListView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        appListView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new AddAppRecyclerViewAdapter(strings);
+        appListView.setAdapter(mAdapter);
 
         return rootView;
     }
---
+++ app/src/main/java/com/prochnow/ttsnotifications/app/AddAppRecyclerViewAdapter.java
@@ -0,0 +1,61 @@
+package com.prochnow.ttsnotifications.app;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by prochnow on 24.06.15.
+ */
+public class AddAppRecyclerViewAdapter extends RecyclerView.Adapter<AddAppRecyclerViewAdapter.ViewHolder> {
+    private List<String> mDataset;
+
+    public AddAppRecyclerViewAdapter(List<String> list) {
+        mDataset = list;
+    }
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+
+        public ViewHolder(View v) {
+            super(v);
+
+            mTextView = (TextView) v;
+
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent,
+                                         int viewType) {
+
+        // create a new view
+        TextView textView = new TextView(parent.getContext());
+        // set the view's size, margins, paddings and layout parameters
+
+        ViewHolder vh = new ViewHolder(textView);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTextView.setText(mDataset.get(position).toString());
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+}
